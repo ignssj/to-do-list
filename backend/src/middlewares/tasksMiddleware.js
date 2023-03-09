cf10306f4229e33e4865d3e5a12130273beb13e4
Ignacio <ign.garcia61@gmail.com>
2023-03-09 17:04:33 -0300

    tasksMiddleware added

---
+++ backend/src/middlewares/tasksMiddleware.js
@@ -0,0 +1,22 @@
+const validateBody = (request, response, next) => {
+    const {body} = request;
+
+    if(body.title === undefined){
+        return response.status(400).json({message: "O titulo é obrigatório"});
+    }
+
+    if(body.title === ""){
+        return response.status(400).json({message: "O titulo não pode ser vazio."});
+    }
+    
+    if(body.status != 0 && body.status != 1){
+        return response.status(400).json({message: "O status deve ser 0 ou 1."});
+    }
+
+    next();
+
+};
+
+module.exports = {
+    validateBody
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const taskController = require("./controllers/taskController");
+const taskMiddleware = require("./middlewares/tasksMiddleware");
 
 router.get("/tasks",taskController.getAll);
-router.post("/tasks",taskController.createTask);
+router.post("/tasks",taskMiddleware.validateBody,taskController.createTask);
 
 module.exports = router;
 
